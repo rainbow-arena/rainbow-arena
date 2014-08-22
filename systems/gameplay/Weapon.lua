30288260517ca25efa404dbb564fa56908c07336
Andrew A <legospacy@gmail.com>
2014-08-23 09:11:55 +1000

    Fixed weapon:firing() vector arguments
    The vectors were being calculated after the function was called, and so
    weren't being passed to the function.

--- systems/gameplay/Weapon.lua
+++ systems/gameplay/Weapon.lua
@@ -19,22 +19,24 @@ return {
 					entity.Weapon.fired = false
 				end
 
-				-- Called every frame while firing or after fired. Useful for beam weapons.
-				if entity.Firing and entity.Weapon.firing then
-					entity.Weapon:firing(world, entity, position_vector, direction_vector, dt)
-				end
-
-				-- Fire if applicable.
-				if entity.Firing and entity.Weapon.heat == 0 then
+				if entity.Firing then
 					local direction_vector = vector.new(
-						math.cos(entity.Rotation), math.sin(entity.Rotation))
+							math.cos(entity.Rotation), math.sin(entity.Rotation))
 					local position_vector = entity.Position:clone()
 						+ (direction_vector * (entity.Radius))
 
-					if (entity.Weapon.type == "single" and not entity.Weapon.fired)
-						or entity.Weapon.type == "repeat" then
-						entity.Weapon:fire(world, entity, position_vector, direction_vector)
-						entity.Weapon.fired = true
+					-- Called every frame while firing or after fired. Useful for beam weapons.
+					if entity.Weapon.firing then
+						entity.Weapon:firing(world, entity, position_vector, direction_vector, dt)
+					end
+
+					-- Fire if applicable.
+					if entity.Weapon.heat == 0 then
+						if (entity.Weapon.type == "single" and not entity.Weapon.fired)
+							or entity.Weapon.type == "repeat" then
+							entity.Weapon:fire(world, entity, position_vector, direction_vector)
+							entity.Weapon.fired = true
+						end
 					end
 				end
 			end
