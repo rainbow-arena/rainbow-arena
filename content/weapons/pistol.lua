b6b43b4f07bf7fd5248c1e0e08fd6b7562ebe74f
Andrew A <legospacy@gmail.com>
2014-08-23 16:29:41 +1000

    Separated projectile logic
    There is now a generic projectile, and a physical one which extends
    that. I still need to create a way to add arbitrary functionality to a
    projectile when it collides.

--- content/weapons/pistol.lua
+++ content/weapons/pistol.lua
@@ -1,7 +1,7 @@
-local projectile = require("content.weapons.logic.projectile")
+local projectile = require("content.weapons.projectiles.physical")
 
 return function(cooldown, bullet_radius, bullet_mass, bullet_speed, bullet_damage, bullet_lifetime)
-	local bullet = projectile.bullet(bullet_radius, bullet_mass, bullet_lifetime, bullet_damage)
+	local bullet = projectile(bullet_radius, bullet_mass, bullet_lifetime, bullet_damage)
 
 	return require("content.weapons.singleshot_projectile")(
 		0.1, bullet_speed, bullet)
---
+++ content/weapons/projectiles/physical.lua
@@ -0,0 +1,22 @@
+local proj_generic = require("content.weapons.projectiles.generic")
+
+local weaputil = require("content.weapons.logic.weaputil")
+local util = require("lib.self.util")
+
+---
+
+return function(radius, mass)
+	return util.table.join(
+		proj_generic(),
+		{
+			Radius = radius,
+			Mass = mass,
+
+			OnProjectileCollision = function(self, world, target, mtv)
+				-- Add impact "force".
+				target.Velocity = weaputil.calculate_post_impact_velocity(
+					self.Mass, self.Velocity, target.Mass, target.Velocity)
+			end
+		}
+	)
+end
