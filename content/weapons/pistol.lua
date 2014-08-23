7a2380f9fbee26a92edaca0792ad7ccf7b58be0a
Andrew A <legospacy@gmail.com>
2014-08-23 18:14:10 +1000

    Added "bullet" projectile; test weapon uses it

--- content/weapons/pistol.lua
+++ content/weapons/pistol.lua
@@ -1,7 +1,7 @@
-local projectile = require("content.weapons.projectiles.physical")
+local projectile = require("content.weapons.projectiles.bullet")
 
-return function(cooldown, bullet_radius, bullet_mass, bullet_speed, bullet_damage, bullet_lifetime)
-	local bullet = projectile(bullet_radius, bullet_mass, bullet_lifetime, bullet_damage)
+return function(cooldown, bullet_radius, bullet_mass, bullet_speed, bullet_damage)
+	local bullet = projectile(bullet_radius, bullet_mass, bullet_damage)
 
 	return require("content.weapons.singleshot_projectile")(
 		0.1, bullet_speed, bullet)
