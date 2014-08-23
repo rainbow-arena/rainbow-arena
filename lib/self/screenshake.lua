9726e1f23cad418c6c15e8b47c82215c1cdd8833
Andrew A <legospacy@gmail.com>
2014-08-23 20:23:00 +1000

    Tweaked and cleaned screenshake files

--- lib/self/screenshake.lua
+++ lib/self/screenshake.lua
@@ -1,9 +1,16 @@
 local screenshake = {}
 
+local floor = math.floor
+
 function screenshake.apply(xamp, yamp)
-  if xamp >= 1 and yamp >= 1 then
-    love.graphics.translate(love.math.random(0, xamp*2)-xamp, love.math.random(0, yamp*2)-yamp)
-  end
+	xamp, yamp = floor(xamp), floor(yamp)
+
+	if xamp >= 1 and yamp >= 1 then
+		love.graphics.translate(
+			love.math.random(0, xamp*2)-xamp,
+			love.math.random(0, yamp*2)-yamp
+		)
+	end
 end
 
 return screenshake
--- systems/graphics/Screenshake.lua
+++ systems/graphics/Screenshake.lua
@@ -2,7 +2,6 @@ local vector = require("lib.hump.vector")
 local util = require("lib.self.util")
 
 local clamp = util.math.clamp
-local floor = math.floor
 
 return {
 	systems = {
@@ -11,7 +10,7 @@ return {
 			requires = {"Position", "Screenshake"},
 			update = function(source, world, dt, camera)
 				local ss = source.Screenshake
-				assert( ss.intensity and ss.falloff, "Screenshake component missing fields" .. (source.Name and (" in entity: " .. source.Name)) )
+				assert( ss.intensity and ss.falloff, "Screenshake component missing field(s)" .. (source.Name and (" in entity: " .. source.Name)) )
 
 				-- Initialise starting time if this is a timed source.
 				if source.Lifetime and not ss.lifetime then
@@ -19,8 +18,7 @@ return {
 				end
 
 				local intensity = ss.intensity
-				-- Adjust timed source intensity (linearly interpolates through lifetime).
-				if source.Lifetime and ss.lifetime then
+				if source.Lifetime and ss.lifetime then -- Adjust timed source intensity.
 					intensity = intensity * (source.Lifetime / ss.lifetime)
 				end
 
@@ -28,7 +26,7 @@ return {
 				local dist_to_source = (source.Position - camera_pos):len()
 
 				-- (Falloff is pixels distance per one intensity level drop.)
-				local final_intensity = floor( clamp(0, intensity - dist_to_source/ss.falloff, math.huge) )
+				local final_intensity = clamp(0, intensity - (dist_to_source/ss.falloff), math.huge)
 
 				camera.screenshake = camera.screenshake + final_intensity
 			end
