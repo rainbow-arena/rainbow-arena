3e29371ffaf5175dc32a3bc2b26601a6fcfd9d73
Andrew A <legospacy@gmail.com>
2014-08-25 12:21:44 +1000

    Added ParticleSystem component support

---
+++ systems/graphics/ParticleSystem.lua
@@ -0,0 +1,22 @@
+return {
+	systems = {
+		{
+			name = "UpdateParticleSystem",
+			requires = {"ParticleSystem"},
+			update = function(entity, world, dt)
+				if entity.ParticleSystem:getCount() == 0 then
+					entity.ParticleSystem = nil
+				else
+					entity.ParticleSystem:update(dt)
+				end
+			end
+		},
+		{
+			name = "DrawParticleSystem",
+			requires = {"ParticleSystem"},
+			update = function(entity, world, dt)
+				love.graphics.draw(entity.ParticleSystem, entity.Position.x, entity.Position.y)
+			end
+		}
+	}
+}
