492389c211aa0857412880377795169d193c7ee5
Andrew A <legospacy@gmail.com>
2014-08-26 20:24:27 +1000

    Updated TODO things

--- main.lua
+++ main.lua
@@ -4,24 +4,12 @@
 
 --[[
 	Stuff to do:
-		Graphics: combatants, background, visual effects (like screenshake), etc.
-		Content: weapons, powerups, etc.
+		Graphics: background
+		Content: weapons, powerups
 		Matches: Actual match/level logic
-		Sound: PlaySound event with position and source?
-
-		Explosions: Explosion event with position and force,
-			or explosion entity has components that specify force?
-
-	TODO:
-		Add Force and/or Damage events?
-		Standardise projectiles better (including use of events).
-			Make a better weapon creation system. Component based?
-		Events: Figure out entities which don't have required components.
+		Sound: PlaySound event/component with position and source?
 
 		Weapons to make:
-			Pistol
-			Minigun
-			Triple barreled machine gun
 			Beam laser
 			Sticky bomb launcher
 			Rocket launcher
