bb0b1769ce73f601fb380525358ae5e04bc6912a
Andrew A <legospacy@gmail.com>
2014-08-31 09:27:26 +1000

    Made collision sounds not use entities

--- systems/audio/Sound.lua
+++ systems/audio/Sound.lua
@@ -24,7 +24,7 @@ return {
 			requires = {"Position", "Sound"},
 			update = function(source, world, dt)
 				local ss = source.Sound
-				assert(ss.source, "Sound component missing field(s)!")
+				assert(ss.source, "Sound component missing source field!")
 
 				ss.source:setPosition(source.Position.x, source.Position.y, 0)
 
@@ -33,10 +33,6 @@ return {
 					pitch = pitch * ss.pitch
 				end
 				ss.source:setPitch(pitch)
-
-				if ss.volume then
-					ss.source:setVolume(ss.volume)
-				end
 
 				if not ss.playing then
 					ss.source:play()
@@ -54,28 +50,22 @@ return {
 		{ -- Sound for arena wall collisions.
 			event = "ArenaCollision",
 			func = function(world, entity, pos, side)
-				world:spawnEntity{
-					Position = pos:clone(),
-					Lifetime = 0.3,
-					Sound = {
-						source = love.audio.newSource(collision_sound),
-						volume = clamp(0, entity.Velocity:len() / volume_threshold_speed, 1)
-					}
-				}
+				local source = love.audio.newSource(collision_sound)
+				source:setVolume( clamp(0, entity.Velocity:len() / volume_threshold_speed, 1) )
+				source:setPosition(pos.x, pos.y, 0)
+				source:play()
 			end
 		},
 		{ -- Sound for entity collision.
 			event = "PhysicsCollision",
 			func = function(world, ent1, ent2, mtv)
 				if can_spawn_col_sound then
-					world:spawnEntity{
-						Position = ent2.Position + mtv,
-						Lifetime = 0.3,
-						Sound = {
-							source = love.audio.newSource(collision_sound),
-							volume = clamp(0, (ent1.Velocity + ent2.Velocity):len() / volume_threshold_speed, 1)
-						}
-					}
+					local source = love.audio.newSource(collision_sound)
+					local pos = ent2.Position + mtv
+					source:setVolume( clamp(0, (ent1.Velocity + ent2.Velocity):len() / volume_threshold_speed, 1) )
+					source:setPosition(pos.x, pos.y, 0)
+					source:play()
+
 					can_spawn_col_sound = false
 					timer.add(sound_spawn_delay, function()
 						can_spawn_col_sound = true
