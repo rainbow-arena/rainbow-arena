5a79507db04cbfbe7082f94512c2b0c782bbed69
Andrew A <legospacy@gmail.com>
2014-09-01 18:53:07 +1000

    Tweaked the shotgun's firing pattern

--- entities/weapons/shotgun.lua
+++ entities/weapons/shotgun.lua
@@ -9,7 +9,7 @@ local w_shotgun = class{__includes = w_projectile}
 
 function w_shotgun:init(arg)
 	self.shotgun_pellets = arg.shotgun_pellets or 5
-	self.shotgun_spread = arg.shotgun_spread or math.pi/4
+	self.shotgun_spread = arg.shotgun_spread or math.pi/8
 
 	w_projectile.init(self, arg)
 end
@@ -17,9 +17,9 @@ end
 function w_shotgun:fire(host, world, pos, dir)
 	for pellet = 1, self.shotgun_pellets do
 		local spread = self.shotgun_spread
-		local angle = (love.math.random() - 0.5) * (spread/2)
+		local angle = (love.math.random() - 0.5) * spread
 
-		self:fire_projectile(host, world, pos, dir:rotated(angle))
+		self:fire_projectile(host, world, pos + dir:perpendicular()*angle*50, dir:rotated(angle))
 	end
 
 	self:apply_shot_effects(host, world, pos, dir)
