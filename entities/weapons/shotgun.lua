05f8539eb7703df298419d50cb64c7ad26e2ef9f
Andrew A <legospacy@gmail.com>
2014-09-01 16:01:25 +1000

    Finished shotgun

--- entities/weapons/shotgun.lua
+++ entities/weapons/shotgun.lua
@@ -9,7 +9,7 @@ local w_shotgun = class{__includes = w_projectile}
 
 function w_shotgun:init(arg)
 	self.shotgun_pellets = arg.shotgun_pellets or 5
-	self.shotgun_spread = arg.shotgun_spread or math.pi/6
+	self.shotgun_spread = arg.shotgun_spread or math.pi/3
 
 	w_projectile.init(self, arg)
 end
@@ -17,7 +17,7 @@ end
 function w_shotgun:fire(host, world, pos, dir)
 	for pellet = 1, self.shotgun_pellets do
 		local spread = self.shotgun_spread
-		local angle = love.math.random(0, spread/2) - spread
+		local angle = (love.math.random() - 0.5) * (spread/2)
 
 		w_projectile.fire(self, host, world, pos, dir:rotated(angle))
 	end
