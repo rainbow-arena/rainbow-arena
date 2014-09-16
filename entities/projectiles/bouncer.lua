63c617cef9e01b8cb1d536ddaaad84095580cfb9
Andrew A <legospacy@gmail.com>
2014-09-16 13:09:34 +1000

    Temp-fixed bouncer double collision
    Now, the bouncer projectile won't collide with the same object twice in
    0.08 seconds. It's hacky, but it's hardly noticable.
    
    If I think of a better way to do this, I will implement it.

--- entities/projectiles/bouncer.lua
+++ entities/projectiles/bouncer.lua
@@ -10,16 +10,25 @@ local e_proj_bouncer = class{__includes = e_proj_physical}
 function e_proj_bouncer:init(arg)
 	arg = arg or {}
 
-	self.bounces = arg.bounces or 1
+	self.bounces = arg.bounces or 3
 	self.CollisionPhysics = true
 
 	e_proj_physical.init(self, arg)
 end
 
 function e_proj_bouncer:on_collision(world, target, mtv)
-	self.bounces = self.bounces - 1
-	if self.bounces < 0 then
-		world:destroy_entity(self)
+	if self.last_bounce_target ~= target then
+		self.bounces = self.bounces - 1
+		self.last_bounce_target = target
+
+		-- Minimise bouncing off the same object twice in rapid succession.
+		world.timer:add(0.08, function()
+			self.last_bounce_target = nil
+		end)
+
+		if self.bounces < 0 then
+			world:destroy_entity(self)
+		end
 	end
 
 	e_proj_physical.on_collision(self, world, target, mtv)
