15e0e165711f516b28ec45a7404289b3a9871796
Andrew A <legospacy@gmail.com>
2014-09-17 10:27:21 +1000

    Added bomb projectile
    It still needs work.

---
+++ entities/projectiles/bomb.lua
@@ -0,0 +1,46 @@
+local util = require("lib.self.util")
+
+---
+
+local class = require("lib.hump.class")
+
+---
+
+local e_proj_physical = require("entities.projectiles.physical")
+local e_proj_bomb = class{__includes = e_proj_physical}
+
+---
+
+function e_proj_bomb:init(arg)
+	arg = arg or {}
+
+	arg.radius = arg.radius or 10
+
+	e_proj_physical.init(self, arg)
+end
+
+function e_proj_bomb:on_collision(world, target, mtv)
+	local exp = require("entities.effects.explosion"){
+		position = self.Position:clone()
+	}
+
+	world:spawn_entity(exp)
+	world:destroy_entity(self)
+
+	e_proj_physical.on_collision(self, world, target, mtv)
+end
+
+function e_proj_bomb:on_arena_collision(world, pos, side)
+	local exp = require("entities.effects.explosion"){
+		position = self.Position:clone()
+	}
+
+	world:spawn_entity(exp)
+	world:destroy_entity(self)
+
+	e_proj_physical.on_arena_collision(self, world, pos, side)
+end
+
+---
+
+return e_proj_bomb
