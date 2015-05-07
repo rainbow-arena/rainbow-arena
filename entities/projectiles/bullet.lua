e4c680f3d609843e60e68115f2991fe800f9c1d6
Andrew Abbott <legospacy@gmail.com>
2015-05-07 17:51:38 +1000

    Start adding projectiles

---
+++ entities/projectiles/bullet.lua
@@ -0,0 +1,11 @@
+local e_bullet = {}
+
+---
+
+function e_bullet.new(damage)
+
+end
+
+---
+
+return e_bullet
---
+++ systems/gameplay/projectiles/PhysicalDamage.lua
@@ -0,0 +1,10 @@
+return {
+	events = {
+		{
+			event = "EntityCollision",
+			func = function(world, ent1, ent2, mtv)
+
+			end
+		}
+	}
+}
