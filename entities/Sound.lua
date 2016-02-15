566340ae4b724d71051f622313cc6c500891eed0
Andrew Abbott <legospacy@gmail.com>
2016-02-15 14:39:28 +1100

    Get sound working

---
+++ entities/Sound.lua
@@ -0,0 +1,45 @@
+--- Require ---
+local Class = require("lib.hump.class")
+local vector = require("lib.hump.vector")
+
+local util = require("lib.util")
+--- ==== ---
+
+
+--- Classes ---
+--- ==== ---
+
+
+--- Class definition ---
+local ent_Sound = {}
+--- ==== ---
+
+
+--- Local functions ---
+--- ==== ---
+
+
+--- Class functions ---
+function ent_Sound:init(template)
+	assert(util.table.check(template, {
+		"Position",
+		"soundpath",
+	}, "Sound"))
+
+	util.table.fill(self, template)
+
+	self.Sound = {
+		source = love.audio.newSource(template.soundpath),
+		volume = template.volume or 1,
+		pitch = template.pitch or 1,
+		removeOnFinish = template.removeOnFinish
+	}
+
+	if template.looping then
+		self.Sound.source:setLooping(true)
+	end
+end
+--- ==== ---
+
+
+return Class(ent_Sound)
